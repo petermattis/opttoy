4176f5308e1d5cd019ca18387c07adfbeb381483
Peter Mattis <petermattis@gmail.com>
2017-11-09 08:26:59 -0500

    Skeleton of pattern operator

--- v3/operator.go
+++ v3/operator.go
@@ -86,25 +86,8 @@ const (
 
 	functionOp
 
-	// TODO(peter): Pattern operators. The idea here is to define patterns in
-	// terms of the expression structure. patternLeaf allows the extraction of an
-	// expression tree for which we only care about the logical properties of the
-	// node. patternTree allows for the recursive extraction of the full
-	// subtree. Any other operator must match precisely.
-	//
-	// Consider a transformation that wants to match on groupByOp ->
-	// innerJoinOp. This would be expressed as:
-	//
-	//   groupByOp
-	//     groupings:     patternTree
-	//     aggregations:  patternTree
-	//     filters:       patternTree
-	//     inputs:
-	//       innerJoinOp
-	//         filters:   patternTree
-	//       inputs:      patternLeaf
-	patternLeaf
-	patternTree
+	// Pattern operator
+	patternOp
 
 	numOperators
 )
---
+++ v3/pattern.go
@@ -0,0 +1,49 @@
+package v3
+
+import "bytes"
+
+// The pattern operator allows defining patterns in terms of the expression
+// structure. A pattern expression is used to extract an actual expression from
+// the memo in order to perform a transformation on it. The pattern operator
+// indicates where recursive extraction of the full subtree is required. For
+// example, the join commutativity transformation wants to extract
+// expressions. It does not care about the inputs, but only wants to reorder
+// them. This can be specified by the pattern expression:
+//
+//   &expr{op: innerJoinOp}
+//
+// Join associativity is somewhat more complicated in that it wants a join
+// where the left input is also a join:
+//
+//   &expr{op: innerJoinOp, inputs; []*expr{{op: innerJoinOp}, nil}}
+//
+// Only the matching portions of the expression are extracted. If the memoExpr
+// contains additional fields such as filters, those are extracted as
+// placeholder expressions with op==patternOp.
+
+func init() {
+	registerOperator(patternOp, "pattern", pattern{})
+}
+
+type pattern struct{}
+
+func (pattern) kind() operatorKind {
+	return relationalKind
+}
+
+func (pattern) format(e *expr, buf *bytes.Buffer, level int) {
+}
+
+func (pattern) initKeys(e *expr, state *queryState) {
+}
+
+func (pattern) updateProps(e *expr) {
+}
+
+func (pattern) requiredInputVars(e *expr) bitmap {
+	return 0
+}
+
+func (pattern) equal(a, b *expr) bool {
+	return true
+}
