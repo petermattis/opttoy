2bfd1528187e24df5db3dbcde28d77a4d7734536
Peter Mattis <petermattis@gmail.com>
2017-11-12 16:24:19 -0500

    Infer IS NOT NULL filters during prep

--- v3/prep.go
+++ v3/prep.go
@@ -2,6 +2,8 @@ package v3
 
 import (
 	"math/bits"
+
+	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 )
 
 // TODO(peter):
@@ -15,13 +17,17 @@ func inferFilters(e *expr) {
 		inferFilters(input)
 	}
 
-	// TODO(peter): infer IS NOT NULL filters.
+	inferEquivFilters(e)
+	inferNotNullFilters(e)
+}
 
+func inferEquivFilters(e *expr) {
 	// For each equivalent column set, look for filters which use on one of the
 	// equivalent columns. If such a filter, is found, create additional filters
 	// via substitution.
 	var inferredFilters []*expr
 	for _, equiv := range e.props.equivCols {
+		var count int
 		for _, filter := range e.filters() {
 			filterInputCols := filter.scalarInputCols()
 			if (filterInputCols & equiv) == 0 {
@@ -30,12 +36,13 @@ func inferFilters(e *expr) {
 			if filterInputCols.count() != 1 {
 				continue
 			}
+			count++
 			filterCol := bitmapIndex(bits.TrailingZeros64(uint64(filterInputCols)))
 			// Loop over the equivalent columns and create new expressions by
 			// substitution.
-			for equiv != 0 {
-				i := bitmapIndex(bits.TrailingZeros64(uint64(equiv)))
-				equiv.clear(i)
+			for v := equiv; v != 0; {
+				i := bitmapIndex(bits.TrailingZeros64(uint64(v)))
+				v.clear(i)
 				if filterCol == i {
 					continue
 				}
@@ -47,6 +54,7 @@ func inferFilters(e *expr) {
 						replacement := input.props.newColumnExprByIndex(i)
 						newFilter := substitute(filter, filterInputCols, replacement)
 						inferredFilters = append(inferredFilters, newFilter)
+						break
 					}
 				}
 			}
@@ -64,6 +72,45 @@ func inferFilters(e *expr) {
 		}
 		if !exists {
 			e.addFilter(inferred)
+		}
+	}
+}
+
+func inferNotNullFilters(e *expr) {
+	// Determine which columns became NOT NULL at this expression (i.e. they were
+	// nullable in the inputs).
+	var inputNotNullCols bitmap
+	for _, input := range e.inputs() {
+		inputNotNullCols.unionWith(input.props.notNullCols)
+	}
+	newNotNullCols := e.props.notNullCols & ^inputNotNullCols
+
+	// Remove any columns for which a filter already exists on only that column
+	// which filters NULLs.
+	for _, filter := range e.filters() {
+		filterInputCols := filter.scalarInputCols()
+		if filterInputCols.count() != 1 {
+			continue
+		}
+		filterCol := bitmapIndex(bits.TrailingZeros64(uint64(filterInputCols)))
+		newNotNullCols.clear(filterCol)
+	}
+
+	// Generate the IS NOT NULL filters for the remaining columns.
+	for v := newNotNullCols; v != 0; {
+		i := bitmapIndex(bits.TrailingZeros64(uint64(v)))
+		v.clear(i)
+
+		// TODO(peter): See comment in inferEquivFilters(). We should be able to
+		// generate from e.props, but currently we can't.
+		for _, input := range e.inputs() {
+			if input.props.outputCols.get(i) {
+				newFilter := newBinaryExpr(isNotOp,
+					input.props.newColumnExprByIndex(i),
+					newConstExpr(tree.DNull))
+				newFilter.updateProps()
+				e.addFilter(newFilter)
+			}
 		}
 	}
 }
