2816afb2573fe0697ecee24d6e1c5e08213510f9
Rebecca Taft <becca@cockroachlabs.com>
2017-12-05 12:43:26 -0500

    Add functions to filter a histogram
    Add functions to filter a histogram given a predicate such as
    x < 4, in which the column is compared to a constant with a
    scalar operator.  The new functions filter the histogram for
    the operators ltOp, leOp, gtOp and geOp. The result is a histogram
    which essentially has a subset of the buckets in the original
    histogram. There is some complexity if the constant in the predicate
    falls in the middle of a bucket, because then the bucket
    must be split into two buckets.

--- v3/planner.go
+++ v3/planner.go
@@ -51,6 +51,57 @@ func (p *planner) exec(stmt tree.Statement) string {
 			p.catalog, tableName(tname.DatabaseName), columnName(tname.TableName), stmt.Rows,
 		)
 		return h.String()
+	case *tree.Select:
+		// This is a statement of the form
+		//   SELECT * from histogram.table.column WHERE ...
+		sel, ok := stmt.Select.(*tree.SelectClause)
+		if !ok {
+			unimplemented("%s", stmt)
+		}
+
+		// Get the histogram name.
+		name, ok := sel.From.Tables[0].(*tree.AliasedTableExpr).Expr.(*tree.NormalizableTableName)
+		if !ok {
+			unimplemented("%s", stmt)
+		}
+		tname, err := name.Normalize()
+		if err != nil {
+			fatalf("unable to normalize: %v", err)
+		}
+		if tname.PrefixName != "histogram" {
+			unimplemented("%s", stmt)
+		}
+
+		// Get the histogram from the catalog.
+		tblName, colName := tableName(tname.DatabaseName), columnName(tname.TableName)
+		tab, ok := p.catalog[tblName]
+		if !ok {
+			fatalf("unable to find table %s", tblName)
+		}
+		colIdx, ok := tab.colMap[colName]
+		if !ok {
+			fatalf("unable to find %s.%s", tblName, colName)
+		}
+		hist := tab.columns[colIdx].hist
+
+		// Filter the histogram.
+		expr, ok := sel.Where.Expr.(*tree.ComparisonExpr)
+		if !ok {
+			unimplemented("%s", stmt)
+		}
+		op := comparisonOpMap[expr.Operator]
+		val, err := expr.Right.(*tree.NumVal).AsInt64()
+		if err != nil {
+			unimplemented("%s", stmt)
+		}
+		switch op {
+		case leOp, ltOp:
+			return hist.filterHistogramLtOpLeOp(op, val).String()
+		case geOp, gtOp:
+			return hist.filterHistogramGtOpGeOp(op, val).String()
+		default:
+			unimplemented("%s", stmt)
+		}
 	default:
 		unimplemented("%T", stmt)
 	}
