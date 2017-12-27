ddab174cf9470d471e0f07d8a290281467503f08
Peter Mattis <petermattis@gmail.com>
2017-12-27 15:13:19 -0500

    Improve subquery typing

--- v3/scope.go
+++ v3/scope.go
@@ -13,6 +13,10 @@ type scope struct {
 	parent *scope
 	props  *relationalProps
 	state  *queryState
+	// Desired number of columns for subqueries found during name resolution and
+	// type checking. This only applies to the top-level subqueries that are
+	// anchored directly to a relational expression.
+	columns int
 }
 
 func (s *scope) push(props *relationalProps) *scope {
@@ -24,22 +28,14 @@ func (s *scope) push(props *relationalProps) *scope {
 }
 
 func (s *scope) resolve(expr tree.Expr, desired types.T) tree.TypedExpr {
+	// TODO(peter): The caller should specify the desired number of columns. This
+	// is needed when a subquery is used by an UPDATE statement.
+	s.columns = 1
+
 	expr, _ = tree.WalkExpr(s, expr)
 	texpr, err := tree.TypeCheck(expr, &s.state.semaCtx, desired)
 	if err != nil {
 		panic(err)
-	}
-
-	// A subquery is only allowed to return a single column in top-level scalar
-	// contexts such as projections. Note that the type of the subquery may be a
-	// tuple (e.g. `SELECT (1, 2)`) even though the subquery itself returns only
-	// a single column.
-	//
-	// TODO(peter): this seems hacky.
-	if sub, ok := texpr.(*subquery); ok {
-		if n := len(sub.expr.props.columns); n != 1 {
-			panic(fmt.Errorf("subquery must return one column, found %d", n))
-		}
 	}
 
 	nexpr, err := s.state.evalCtx.NormalizeExpr(texpr)
@@ -59,7 +55,8 @@ func (s *scope) newVariableExpr(idx int) *expr {
 	return nil
 }
 
-// NB: This code is adapted from sql/select_name_resolution.go.
+// NB: This code is adapted from sql/select_name_resolution.go and
+// sql/subquery.go.
 func (s *scope) VisitPre(expr tree.Expr) (recurse bool, newExpr tree.Expr) {
 	switch t := expr.(type) {
 	case *tree.IndexedVar:
@@ -130,32 +127,33 @@ func (s *scope) VisitPre(expr tree.Expr) (recurse bool, newExpr tree.Expr) {
 			}
 		}
 
-	case *tree.Subquery:
-		return false, &subquery{
-			table: false,
-			expr:  build(t.Select, s),
-		}
-
 	case *tree.ArrayFlatten:
+		// TODO(peter): the ARRAY flatten operator requires a single column from
+		// the subquery.
 		if sub, ok := t.Subquery.(*tree.Subquery); ok {
-			t.Subquery = &subquery{
-				table: true,
-				expr:  build(sub.Select, s),
-			}
+			t.Subquery = s.replaceSubquery(sub, true /* multi-row */, 1 /* desired-columns */)
 		}
 
 	case *tree.ComparisonExpr:
 		switch t.Operator {
 		case tree.In, tree.NotIn, tree.Any, tree.Some, tree.All:
 			if sub, ok := t.Right.(*tree.Subquery); ok {
-				t.Right = &subquery{
-					table: true,
-					expr:  build(sub.Select, s),
-				}
+				t.Right = s.replaceSubquery(sub, true /* multi-row */, -1 /* desired-columns */)
 			}
 		}
+
+	case *tree.ExistsExpr:
+		if sub, ok := t.Subquery.(*tree.Subquery); ok {
+			t.Subquery = s.replaceSubquery(sub, true /* multi-row */, -1 /* desired-columns */)
+		}
+
+	case *tree.Subquery:
+		expr = s.replaceSubquery(t, false /* multi-row */, s.columns /* desired-columns */)
 	}
 
+	// Reset the desired number of columns since if the subquery is a child of
+	// any other expression, type checking will verify the number of columns.
+	s.columns = -1
 	return true, expr
 }
 
@@ -163,12 +161,39 @@ func (*scope) VisitPost(expr tree.Expr) tree.Expr {
 	return expr
 }
 
+// Replace a raw subquery node with a typed subquery. multiRow specifies
+// whether the subquery is occurring in a single-row or multi-row
+// context. desiredColumns specifies the desired number of columns for the
+// subquery. Specifying -1 for desiredColumns allows the subquery to return any
+// number of columns and is used when the normal type checking machinery will
+// verify that the correct number of columns is returned.
+func (s *scope) replaceSubquery(sub *tree.Subquery, multiRow bool, desiredColumns int) *subquery {
+	result := build(sub.Select, s)
+	if desiredColumns > 0 && len(result.props.columns) != desiredColumns {
+		n := len(result.props.columns)
+		switch desiredColumns {
+		case 1:
+			panic(fmt.Errorf("subquery must return one column, found %d", n))
+		default:
+			panic(fmt.Errorf("subquery must return %d columns, found %d", desiredColumns, n))
+		}
+	}
+
+	return &subquery{
+		multiRow: multiRow,
+		expr:     result,
+	}
+}
+
 type subquery struct {
 	typ types.T
-	// Is the subquery in a table context or a scalar context.
-	table bool
-	expr  *expr
+	// Is the subquery in a multi-row or single-row context?
+	multiRow bool
+	expr     *expr
 }
+
+var _ tree.TypedExpr = &subquery{}
+var _ tree.VariableExpr = &subquery{}
 
 // String implements the tree.Expr interface.
 func (s *subquery) String() string {
@@ -190,33 +215,51 @@ func (s *subquery) TypeCheck(_ *tree.SemaContext, desired types.T) (tree.TypedEx
 		return s, nil
 	}
 
-	if s.table {
-		// The subquery is in a "table" context. For example:
-		//
-		//   SELECT 1 IN (SELECT * FROM t)
-		t := &types.TTable{
-			Cols:   make(types.TTuple, len(s.expr.props.columns)),
-			Labels: make([]string, len(s.expr.props.columns)),
+	wrap := true
+	if len(s.expr.props.columns) == 1 {
+		if !s.multiRow {
+			// The subquery has only a single column and is in a single-row context,
+			// we don't want to wrap the type in a tuple. For example:
+			//
+			//   SELECT (SELECT 1)
+			//
+			// and
+			//
+			//   SELECT (SELECT (1, 2))
+			//
+			// This will result in the types "int" and "tuple{int,int}" respectively.
+			wrap = false
+		} else if !types.FamTuple.FamilyEqual(s.expr.props.columns[0].typ) {
+			// The subquery has only a single column and is in a multi-row
+			// context. We only wrap if the type of the result column is not a
+			// tuple. For example:
+			//
+			//   SELECT 1 IN (SELECT 1)
+			//
+			// The type of the subquery will be "tuple{int}". Now consider the
+			// semantically invalid query:
+			//
+			//   SELECT (1, 2) IN (SELECT (1, 2))
+			//
+			// We want the type of subquery to be "tuple{tuple{tuple{int,int}}}" in
+			// order to distinguish it from the semantically valid:
+			//
+			//   SELECT (1, 2) IN (SELECT 1, 2)
+			//
+			// In this query, the subquery has the type "tuple{tuple{int,int}}"
+			// making the IN expression valid.
+			//
+			// Lastly, note that for the multi-row case, there may be multiple
+			// rows. For example:
+			//
+			//   SELECT 1 IN (VALUES (1), (2), (3))
+			//
+			// The subquery returns 3 rows and the type is "tuple{int}".
+			wrap = false
 		}
-		for i := range s.expr.props.columns {
-			t.Cols[i] = s.expr.props.columns[i].typ
-			t.Labels[i] = string(s.expr.props.columns[i].name)
-		}
-		// TODO(peter): This should be `s.typ = t`, but doing that causes the query
-		// `SELECT 1 IN (SELECT 1)` to fail with:
-		//
-		//   unsupported comparison operator: <int> IN <setof tuple{int}>
-		s.typ = t.Cols
-		return s, nil
 	}
 
-	// The subquery is in a scalar context. For example:
-	//
-	//   SELECT (1, 2) = (SELECT 1, 2)
-	//
-	// If the subquery has a single column
-	// we use that as our column type. Otherwise, create a tuple type.
-	if len(s.expr.props.columns) == 1 {
+	if !wrap {
 		s.typ = s.expr.props.columns[0].typ
 	} else {
 		t := make(types.TTuple, len(s.expr.props.columns))
@@ -225,6 +268,22 @@ func (s *subquery) TypeCheck(_ *tree.SemaContext, desired types.T) (tree.TypedEx
 		}
 		s.typ = t
 	}
+
+	if s.multiRow {
+		// The subquery is in a multi-row context. For example:
+		//
+		//   SELECT 1 IN (SELECT * FROM t)
+		//
+		// Wrap the type in a tuple.
+		s.typ = types.TTuple{s.typ}
+	} else {
+		// The subquery is in a scalar context. For example:
+		//
+		//   SELECT (1, 2) = (SELECT 1, 2)
+		//
+		// Nothing more to do here, the type computed above is sufficient.
+	}
+
 	return s, nil
 }
 
